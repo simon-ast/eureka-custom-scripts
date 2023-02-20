7582e0aeb517a2b1f494aae4118f39907c0d2381
Simon <simon.schleich@univie.ac.at>
2023-02-20 17:14:27 +0100

    WLfitInit

---
+++ eureka_custom_plots/eucus_stage5_whitelight-fitting.py
@@ -0,0 +1,25 @@
+"""
+OVERVIEW:
+
+TBD
+
+"""
+
+"""
+PSEUDOCODE
+Use a sampling routine (see also individual light curve fitting) to fit
+parameters for white light curve.
+- The LC model can be used from 'batman'
+- Sampling can be done with e.g. 'dynesty'
+- Should the result in all parameters needed for multi-channel LC fit
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
+    print("PLEASE CITE THE DYNESTY AND BATMAN PUBLICATIONS!")
+
