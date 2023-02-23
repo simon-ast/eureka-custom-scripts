e3c0a6a2c9f58a3fbb7185a8338a08af16b17b18
simon-ast <simon.schleich@univie.ac.at>
2023-02-23 09:06:46 +0100

    PrecisionPlot

--- eureka_custom_plots/eucus_stage3_2d-lightcurve.py
+++ eureka_custom_plots/eucus_stage3_2d-lightcurve.py
@@ -5,11 +5,14 @@ Customised plotting routine for 2D (dynamic) light curves from the Stage 3
 data products produced by 'Eureka!'. This routine reads from the
 'S3*_SpecData.hdf5' files produced in that stage.
 
+Also includes precision plot for MAD [ppm].
+
 """
 
 import h5py as h5
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.optimize import curve_fit
 from util import rc_setup
 
 # DIRECTORIES RELATIVE TO "eureka_custom_plots"!
@@ -36,11 +39,9 @@ def main():
     n_pix = np.arange(spec.shape[1]) + 1    # Should start at 1
     wavel = data["wave_1d"][()]
 
-    # Plot and save dynamic light curve
+    # Plot and save dynamic light curve and precision plot
     plot_dynamic_lc(wavel, n_int, normspec, "RdYlBu_r", "wavelength")
-
-    # Show all generated plots
-    plt.show()
+    plot_precision(wavel, normspec)
 
 
 def plot_dynamic_lc(x_array, y_array, z_array,
@@ -74,6 +75,57 @@ def plot_dynamic_lc(x_array, y_array, z_array,
         f"{plot_loc}/eucus_2Dlightcurve_{save_append}.{PLOT_TYPE}",
         dpi=600
     )
+
+
+def plot_precision(wavelength, norm_flux):
+    """DOC!"""
+    fig, ax = plt.subplots(figsize=(6, 3))
+
+    # Basic parameters
+    n_int, n_pix = norm_flux.shape
+
+    # Find wavelength cutoff at 5.3 microns (don't forget that list
+    # slicing is exclusive for the stopping index!)
+    end_idx = np.where(wavelength > 5.3)[0][0]
+    wavelength = wavelength[:end_idx]
+
+    # Deviation from next neighbour of every point in every column
+    # Restricted by wavelength cutoff
+    ediff1d = np.array([
+        np.ediff1d(norm_flux[:, i]) for i in range(n_pix)
+    ])[:end_idx]
+
+    # Median Absolute Deviation in PPM
+    mad_array = np.ma.median(np.abs(ediff1d), axis=1) * 1e6
+
+    # Plot everything nice and tidy
+    ax.scatter(wavelength, mad_array / 1e3, color="tab:blue", s=15)
+    ax.set(xlabel="Wavelength [$\\mu \\mathrm{m}$]",
+           ylabel="MAD [ppt]")
+
+    # Fit quadratic function and plot
+    #result = curve_fit(exponential, wavelength, mad_array)
+    #a_opt, b_opt, c_opt = result[0]
+    #ax.plot(wavelength, exponential(wavelength, a_opt, b_opt, c_opt),
+    #        c="red", ls="--")
+
+    plt.tight_layout()
+    plot_loc = f"{PLOT_DIR}/stage3_2d-lightcurve"
+    plt.savefig(f"{plot_loc}/eucus_precision.{PLOT_TYPE}", dpi=600)
+
+    # PLOT NOTED COLUMNS
+    #idx_bad = [7, 12, 14, 26, 40, 48, 69]
+    #ax.scatter(wavelength[idx_bad], mad_array[idx_bad], c="red")
+
+
+def quadratic(x, a, b, c):
+    """Function used for fitting"""
+    return a * x ** 2 + b * x + c
+
+
+def exponential(x, a, b, c):
+    """Function used for fitting"""
+    return a * np.exp(-b * x) + c
 
 
 if __name__ == "__main__":
